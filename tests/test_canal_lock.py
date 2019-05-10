126103cbd0f36807e74948681349d145c9b1c32f
Bryant Eisenbach <fubuloubu@gmail.com>
2019-05-09 20:08:13 -0400

    refactor: Changed variables in contract for clarity

--- tests/test_canal_lock.py
+++ tests/test_canal_lock.py
@@ -6,26 +6,26 @@ from hypothesis_ethereum import InstrumentedContract, invariant
 
 
 interface = vyper.compile_code("""
-gate1: public(bool) # False = Up (stopping water), True = Down (letting water through)
-gate2: public(bool) # False = Up (stopping water), True = Down (letting water through)
+gate1_down: public(bool) # False = Up (stopping water), True = Down (letting water through)
+gate2_down: public(bool) # False = Up (stopping water), True = Down (letting water through)
 
 @public
 def raise_gate(pick_gate1: bool):
     if pick_gate1:
-        self.gate1 = False # Raise up, stopping the flow of water
+        self.gate1_down = False # Raise up, stopping the flow of water
     else:
-        self.gate2 = False # Raise up, stopping the flow of water
+        self.gate2_down = False # Raise up, stopping the flow of water
 
 @public
 def lower_gate(pick_gate1: bool):
     if pick_gate1: # Gate 2 cannot
         # Both gates can not be lowered at the same time
-        assert not self.gate2
-        self.gate1 = True # Lower down, allowing the flow of water
+        assert not self.gate2_down
+        self.gate1_down = True # Lower down, allowing the flow of water
     else:
         # Both gates can not be lowered at the same time
-        assert not self.gate1
-        self.gate2 = True # Lower down, allowing the flow of water
+        assert not self.gate1_down
+        self.gate2_down = True # Lower down, allowing the flow of water
 """, output_formats=['abi', 'bytecode', 'bytecode_runtime'])
 
 
