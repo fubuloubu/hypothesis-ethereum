ef54a3e8627168530244f3a08eaa3431c93570b1
Bryant Eisenbach <fubuloubu@gmail.com>
2019-05-18 21:21:38 -0400

    refactor: Read from file instead

--- tests/test_canal_lock.py
+++ tests/test_canal_lock.py
@@ -1,32 +1,13 @@
-import pytest
-
 import vyper
 
+with open('tests/CanalLock.vy', 'r') as f:
+    code = f.read()
+    interface = vyper.compile_code(code,
+            output_formats=['abi', 'bytecode', 'bytecode_runtime'])
+
+
+# This is how the public API should be used
 from hypothesis_ethereum import build_test
-
-
-interface = vyper.compile_code("""
-gate1_down: public(bool) # False = Up (stopping water), True = Down (letting water through)
-gate2_down: public(bool) # False = Up (stopping water), True = Down (letting water through)
-
-@public
-def raise_gate(pick_gate1: bool):
-    if pick_gate1:
-        self.gate1_down = False # Raise up, stopping the flow of water
-    else:
-        self.gate2_down = False # Raise up, stopping the flow of water
-
-@public
-def lower_gate(pick_gate1: bool):
-    if pick_gate1: # Gate 2 cannot
-        # Both gates can not be lowered at the same time
-        assert not self.gate2_down
-        self.gate1_down = True # Lower down, allowing the flow of water
-    else:
-        # Both gates can not be lowered at the same time
-        assert not self.gate1_down
-        self.gate2_down = True # Lower down, allowing the flow of water
-""", output_formats=['abi', 'bytecode', 'bytecode_runtime'])
 
 
 def check_gates_both_down(contract):
